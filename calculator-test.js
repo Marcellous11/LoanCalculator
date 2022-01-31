5f0978bf5f55d46750bc3abe43746971cad29e66
Marcellous Curtis <1mcj001@gmail.com>
2022-01-31 14:59:15 -0600

    start

---
+++ calculator-test.js
@@ -0,0 +1,16 @@
+describe('monthly interest rates', () => { 
+  
+it('should calculate the monthly rate correctly', function () {
+  // ...
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  // ..
+});
+
+/// etc
+
+
+})
+
