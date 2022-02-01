4aadd1ebdcac4dc1a7d57492a232d2b58b0653a7
Marcellous Curtis <1mcj001@gmail.com>
2022-01-31 23:09:27 -0600

    okay i think everything is solved

--- calculator-test.js
+++ calculator-test.js
@@ -1,16 +1,29 @@
-describe('monthly interest rates', () => { 
-  
-it('should calculate the monthly rate correctly', function () {
-  // ...
+describe("monthly interest rates", () => {
+  it("should calculate the monthly rate correctly", function () {
+    const values = {
+        amount: 50000,
+        years : 15,
+        rate: 4.3
+    };
+    expect(calculateMonthlyPayment(values)).toEqual('377.41')
+  });
+
+  it("should return a result with 2 decimal places", function () {
+    const values = {
+      amount: 643221,
+      years : 15,
+      rate: 4.3
+    };
+  expect(calculateMonthlyPayment(values)).toEqual('4855.11')
+  });
+
+  it("should take care of very low interest rate", function () {
+    const values = {
+      amount: 50000,
+      years : 15,
+      rate: 0.0001
+    };
+  expect(calculateMonthlyPayment(values)).toEqual('277.78')
+  });
+
 });
-
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
-
-
-})
-
